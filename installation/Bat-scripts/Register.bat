6ae40fc1317c9eaf2a520f69f91993aa13d71cac
EddyAlleman <979361+EddyAlleman@users.noreply.github.com>
2021-09-16 17:30:44 +0200

    added codebase

--- installation/Bat-scripts/Register.bat
+++ installation/Bat-scripts/Register.bat
@@ -1,3 +1,1 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe  "C:\temp\EquiDistAddin.dll"
-
-
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /codebase "C:\temp\EquiDistAddin.dll"
