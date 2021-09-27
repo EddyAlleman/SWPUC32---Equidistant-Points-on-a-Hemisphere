ff82834f516995d7af469f97732be15932a36e2f
EddyAlleman <979361+EddyAlleman@users.noreply.github.com>
2021-09-27 07:46:11 +0200

    Added support for Coordinate Systems on points

--- EquiDistAddin/EquiDistAddin/CSAddin.cs
+++ EquiDistAddin/EquiDistAddin/CSAddin.cs
@@ -4,28 +4,29 @@
 //Using XCAD from Xarial
 //*********************************************************************
 
-using EquiDistAddin.Properties;
 using System;
-using static System.Environment;
+using System.Drawing;
+using System.Diagnostics;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using Xarial.XCad.Base.Attributes;
-using Xarial.XCad.UI.Commands.Attributes;
-using Xarial.XCad.UI.Commands;
-
-using Xarial.XCad.SolidWorks;
-using Xarial.XCad.UI.PropertyPage;
-
-using Xarial.XCad.UI.PropertyPage.Enums;
-using System.Drawing;
-
-using Xarial.XCad.UI.Commands.Enums;
-using Xarial.XCad.SolidWorks.UI.PropertyPage;
-using System.Diagnostics;
-using Xarial.XCad.UI.PropertyPage.Attributes;
-using Xarial.XCad.Base.Enums;
-using SolidWorks.Interop.sldworks;
 using System.Windows.Media.Media3D;
+using static System.Environment;
+
+using Xarial.XCad.Base.Enums;
+using Xarial.XCad.Base.Attributes;
+using Xarial.XCad.UI.Commands;
+using Xarial.XCad.UI.Commands.Enums;
+using Xarial.XCad.UI.Commands.Attributes;
+using Xarial.XCad.UI.PropertyPage;
+using Xarial.XCad.UI.PropertyPage.Enums;
+using Xarial.XCad.UI.PropertyPage.Attributes;
+using Xarial.XCad.SolidWorks;
+using Xarial.XCad.SolidWorks.UI.PropertyPage;
+
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+using EquiDistAddin.Properties;
 
 namespace EquiDistAddin
 {
@@ -38,55 +39,54 @@ namespace EquiDistAddin
     [CommandGroupInfo(10)]
     public enum Commands_ed
     {
+
         [Title("Equidistant points on sphere")]
         [Description("Creates Equidistant points on a sphere")]
         [Icon(typeof(Resources), nameof(Resources.points_simple))]
         [CommandItemInfo(true, true, WorkspaceTypes_e.Part, true)]
-        CommandB1,
+        CommandButton1,
 
         [Title("Info")]
         [Description("Documentation and Credits")]
         [Icon(typeof(Resources), nameof(Resources.about))]
-        CommandB2
+        CommandButton2
 
     }
+    //---
 
-    //--- Define the sphere optionboxes
+    //--- Define the sphere option boxes
     public enum Options_sphere
     {
+
         [Title("Hemisphere")]
-        //[BitmapOptions(18, 18)]
-        [Icon(typeof(Resources), nameof(Resources.hemisphere))]
         Hemisphere,
 
         [Title("Sphere")]
-        //[BitmapOptions(18, 18)]
-        [Icon(typeof(Resources), nameof(Resources.sphere))]
         Sphere
 
     }
-
+    //---
     #endregion
 
-    //--- Define the PropertyManager Page
+
     #region PMP
 
-    //define the controls you want in the PMP
+    //--- Define the PropertyManager Page
     public class DataModelSphereOptions
     {
 
         [BitmapOptions(220, 220)]
         public Image BitmapLarge { get; set; } = Resources.sphere_illustration;
 
-        [NumberBoxOptions(NumberBoxUnitType_e.Length, 0.1, 10.0, 0.1, true, 1, 0.1, NumberBoxStyle_e.Thumbwheel)]//in m!
+        [NumberBoxOptions(NumberBoxUnitType_e.Length, 0.1, 10.0, 0.1, true, 1, 0.1, NumberBoxStyle_e.Thumbwheel)]//in meter!
         [Description("Radius of the sphere in mm")]
         [StandardControlIcon(BitmapLabelType_e.Radius)]
         public double Radius { get; set; } = 1.0;
 
-        [NumberBoxOptions(NumberBoxUnitType_e.UnitlessInteger, 1, 6, 1, true, 2, 1, NumberBoxStyle_e.Slider)]
+        [NumberBoxOptions(NumberBoxUnitType_e.UnitlessInteger, 0, 6, 1, true, 2, 1, NumberBoxStyle_e.Slider)]
         [Description("Number of recursions. This is how many times we want to divide the distance of the points. The image above shows the result with 2 recursions")]
         [StandardControlIcon(BitmapLabelType_e.CircularPattern)]
-        public int Recursions { get; set; } = 1;
+        public int Recursions { get; set; } = 0;
 
 
         public class DataGroupInput
@@ -95,63 +95,56 @@ namespace EquiDistAddin
             [Title("Sphere type")]
             [Description("Choose a Hemisphere or generate the complete Sphere")]
             [OptionBox]
-            //put the following separately for better code readability
-            //public enum Options_sphere
-            //{
-            //    Hemisphere,
-            //    Sphere
-            //}
             [ComboBoxOptions(ComboBoxStyle_e.Sorted)]
             public Options_sphere SphereOptions { get; set; } = Options_sphere.Hemisphere;
-
-            ////TODO ADD ACTIONS
-            //[BitmapButton(typeof(Resources), nameof(Resources.hemisphere))]
-            //public bool ToggleHemisphere { get; set; } = true;
-
-            //[BitmapButton(typeof(Resources), nameof(Resources.sphere))]
-            //public bool ToggleSphere { get; set; } = false;
 
             [Title("Draw Normals")]
             [Description("Draw the normals on the sphere for every point")]
             [Icon(typeof(Resources), nameof(Resources.normals_sphere))]
-            //[ControlOptions(height: 16, width: 16)]//doesn't do anything
             public bool NormalsChecked { get; set; } = false;
+
+            [Title("Create Coordinate Systems")]
+            [Description("Draw a Coordinate System on the sphere for every point")]
+            [Icon(typeof(Resources), nameof(Resources.CoordinateSystem))]
+            public bool CS_Checked { get; set; } = false;
 
         }
 
         [Title("Options")]
         public DataGroupInput Group1 { get; set; }
 
-
-        ////draw spike
     }
+    //---
 
-    //PMP handler
+    //--- PMP handler
     [ComVisible(true)]
     [Title("Equidistant points on sphere")]
+    [Help("https://www.edalsolutions.be/index.php/nl-NL/automatisatie/addins")]
     public class MyPMPageData : SwPropertyManagerPageHandler
     {
+
         [Title("Dimensions")]
         public DataModelSphereOptions InputSpherePoints { get; set; }
 
     }
-
+    //---
     #endregion
 
-    //---
+
+
     #region ADDIN
 
     [ComVisible(true), Guid("15764d0d-35fb-419c-b1c0-54821484f5d5")]
     [Icon(typeof(Resources), nameof(Resources.swcomaddinwizard))]
     [Title("EquiDistant Points Addin")]
-    [Description("SWPUC32 - Equidistant Points on a Hemisphere (The Thomson Problem)")]
+    [Description("SWPUC32 - Equidistant Points on a Hemisphere")]
     public class AddIn : SwAddInEx
     {
-        //--- private members
+
         private IXPropertyPage<MyPMPageData> m_Page;
         private MyPMPageData m_Data;
 
-        //--- On starting SolidWorks
+        //--- On connecting to SolidWorks
         public override void OnConnect()
         {
 
@@ -160,20 +153,21 @@ namespace EquiDistAddin
             //initialize the PMP data
             m_Data = new MyPMPageData();
 
-
         }
+        //---
 
         //--- handle the command manager buttons
         private void OnCommandClick(Commands_ed spec)
         {
+
             switch (spec)
             {
-                case Commands_ed.CommandB1:
+                case Commands_ed.CommandButton1:
                     m_Page = this.CreatePage<MyPMPageData>();
                     ShowPmpPage();
                     break;
 
-                case Commands_ed.CommandB2:
+                case Commands_ed.CommandButton2:
                     string msg = "The tool creates a 3Dsketch and puts points on the surface of a hemisphere." + NewLine +
                                   NewLine +
                                  "The points are calculated so that they get an ideal equidistant gap between them." + NewLine +
@@ -187,15 +181,15 @@ namespace EquiDistAddin
                                  "tweaked code from the following website: " + NewLine +
                                  "http://blog.andreaskahler.com" + NewLine +
                                   NewLine +
-                                 "Author: Eddy Alleman - https://edalsolutions.be/";
+                                 "Author: Eddy Alleman - https://www.edalsolutions.be/index.php/nl-NL/automatisatie/addins";
 
                     Application.ShowMessageBox(msg, MessageBoxIcon_e.Info, MessageBoxButtons_e.Ok);
-                    // Application.ShowMessageBox("Button2");
                     break;
 
             }
 
         }
+        //---
 
         //--- Helpers
         private void ShowPmpPage()
@@ -203,41 +197,41 @@ namespace EquiDistAddin
             m_Page.Closed += OnPageClosed;
             m_Page.Show(m_Data);
         }
+        //---
 
-        //--- debugging
+        //--- Create
         private void OnPageClosed(PageCloseReasons_e reason)
         {
-
 
             if (reason == PageCloseReasons_e.Okay)
             {
 
                 //get input from PMP
                 double rad = m_Data.InputSpherePoints.Radius;
-                Debug.Print($"Radius: {rad}");
 
                 int recur = m_Data.InputSpherePoints.Recursions;
-                Debug.Print($"Recursions: {recur}");
 
                 Options_sphere hemi = m_Data.InputSpherePoints.Group1.SphereOptions;
-                Debug.Print($"Type of sphere: {hemi}");
 
                 bool normals = m_Data.InputSpherePoints.Group1.NormalsChecked;
-                Debug.Print($"Normals on sphere: {normals}");
+
+                bool coordinatesystems = m_Data.InputSpherePoints.Group1.CS_Checked;
 
 
                 ISldWorks swApp = this.Application.Sw;
 
                 ModelDoc2 swModel = (ModelDoc2)swApp.ActiveDoc;
 
+
                 if (swModel is PartDoc)
                 {
+
                     IcoSphereCreator icoSphere = new IcoSphereCreator();
 
                     MeshGeometry3D meshData = new MeshGeometry3D();
                     meshData = icoSphere.Create(rad, recur);//tested up to 5
 
-                    //get vertex posiitons
+                    //get vertex positions
                     Point3DCollection positions = meshData.Positions;
 
                     int nPositions = positions.Count;
@@ -246,22 +240,33 @@ namespace EquiDistAddin
                     sketchMgr.AddToDB = true;
                     sketchMgr.Insert3DSketch(true);
 
+
                     CreatePointsAndNormals(rad, hemi, normals, positions, nPositions, sketchMgr);
 
                     //close 3Dsketch
                     sketchMgr.InsertSketch(true);
+
+
+                    if (coordinatesystems)
+                    {
+
+                        CreateCoordinateSystems(swModel);
+
+                    }
 
                     swModel.ViewZoomtofit2();
                 }
 
             }
 
+
             #endregion
 
         }
 
-        private static void CreatePointsAndNormals(double rad, Options_sphere hemi, bool normals, Point3DCollection positions, int nPositions, SketchManager sketchMgr)
+        private void CreatePointsAndNormals(double rad, Options_sphere hemi, bool normals, Point3DCollection positions, int nPositions, SketchManager sketchMgr)
         {
+
             //normals length factor to radius
             double factor = 1.2 * rad;
 
@@ -274,18 +279,61 @@ namespace EquiDistAddin
 
                 if (positions[i].Y >= ceiling)
                 {
+                    //always draw points, because we need them for the coordinate systems
+                    SketchPoint pt = sketchMgr.CreatePoint(rad * positions[i].X, rad * positions[i].Y, rad * positions[i].Z);
 
                     if (normals)
                     {
                         sketchMgr.CreateLine(rad * positions[i].X, rad * positions[i].Y, rad * positions[i].Z, factor * positions[i].X, factor * positions[i].Y, factor * positions[i].Z);
                     }
-                    else
-                    {
-                        SketchPoint pt = sketchMgr.CreatePoint(rad * positions[i].X, rad * positions[i].Y, rad * positions[i].Z);
-                    }
+
                 }
             }
         }
 
+        private void CreateCoordinateSystems(ModelDoc2 swModel)
+        {
+            //get last inserted sketch
+            Feature feat = swModel.Extension.GetLastFeatureAdded();
+
+
+            // iterate points and add Coordinate System
+            if (feat?.GetSpecificFeature2() is ISketch sketch)
+            {
+
+                //get all points in sketch
+                object[] points = sketch.GetSketchPoints2() as object[];
+
+                //set a limit for recursion levels 4 and higher
+                long max = 1000;
+                long qty = points.Length > max ? max : points.Length;
+
+                //iterate and select
+                for (int i = 0; i < qty; i++)
+                {
+                    SketchPoint point = points[i] as SketchPoint;
+
+                    bool PointIsSelected = point.Select2(false, 1);
+
+                    if (PointIsSelected)
+                    {
+
+                        //use origin as Z-axis
+                        bool OriginIsSelected = swModel.Extension.SelectByID2("Point1@Origin", "EXTSKETCHPOINT", 0, 0, 0, true, 8, null, 0);
+
+                        if (OriginIsSelected)
+                        {
+                            swModel.InsertCoordinateSystem(false, false, true);
+                        }
+
+                    }
+
+
+                }
+
+
+            }
+        }
+        //---
     }
 }
